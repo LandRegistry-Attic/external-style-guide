ddba8140db94da484889494fcc9c6bcc757d62b9
Demotive <mat@demotive.com>
2014-07-15 12:54:38 +0100

    Adding grunt-contrib-watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,18 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     /*sass: {
+      dev: {
+        options: {
+            style: 'expanded'
+        },
+        files: {
+          'main.css': 'main.scss'
+        }
+      },
       dist: {
+        options: {
+            style: 'compressed'
+        },
         files: {
           'main.css': 'main.scss'
         }
@@ -20,6 +31,16 @@ module.exports = function(grunt) {
       }
     },*/
 
+    /*watch: {
+      css: {
+        files: ['path-to-files'],
+        tasks: ['sass:dev'],
+        options: {
+          spawn: false,
+        }
+      }
+    },*/
+
   });
 
   // Load the plugin that provides the "sass" task: https://github.com/gruntjs/grunt-contrib-sass
@@ -28,7 +49,11 @@ module.exports = function(grunt) {
   // css-min task - used to minify straight css files (i.e. the govuk_template files): https://github.com/gruntjs/grunt-contrib-cssmin
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
+
+  // watch task. Does what it says on the tin: https://github.com/gruntjs/grunt-contrib-watch
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   // Register the various Grunt commands:
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['watch']);
 
 };
