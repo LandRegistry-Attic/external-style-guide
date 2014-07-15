4427c123ce64a8503802e50ef29f563a07f236a5
Demotive <mat@demotive.com>
2014-07-15 12:39:54 +0100

    Scaffolding for grunt-contrib-cssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,12 +11,22 @@ module.exports = function(grunt) {
           'main.css': 'main.scss'
         }
       }
-    }*/
+    },*/
+
+    /*cssmin: {
+      css: {
+        src: 'foo.css',
+        dest: 'bar.css'
+      }
+    },*/
 
   });
 
   // Load the plugin that provides the "sass" task: https://github.com/gruntjs/grunt-contrib-sass
   grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // css-min task - used to minify straight css files (i.e. the govuk_template files): https://github.com/gruntjs/grunt-contrib-cssmin
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Register the various Grunt commands:
   grunt.registerTask('default', ['sass']);
