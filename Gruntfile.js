fb696fd2e449653cb9330fee0d2cbe4331d6ebcc
Demotive <mat@demotive.com>
2014-07-16 14:39:22 +0100

    Adding copy, uglify tasks, building up 'grunt build'

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,18 @@
 module.exports = function(grunt) {
 
   var globalConfig = {
+    govuk_template: {
+      css_dev: 'app/static/development/govuk-template/stylesheets/',
+      js_dev: 'app/static/development/govuk-template/javascripts/',
+      img_dev: 'app/static/development/govuk-template/images/'
+    },
     scss: {
-      dev: 'app/static/development/stylesheets/',
-      build: 'app/static/build/stylesheets/'
+      dev: 'app/static/development/stylesheets/'
+    },
+    build: {
+      css: 'app/static/build/stylesheets/',
+      js: 'app/static/build/javascripts/',
+      img: 'app/static/build/images/'
     }
   };
 
@@ -28,17 +37,50 @@ module.exports = function(grunt) {
             style: 'compressed'
         },
         files: {
-          '<%= globalConfig.scss.build %>landregistry_main.css': '<%= globalConfig.scss.dev %>landregistry_main.scss'
+          '<%= globalConfig.build.css %>landregistry_main.css': '<%= globalConfig.scss.dev %>landregistry_main.scss'
         }
       }
     },
 
-    /*cssmin: {
-      css: {
-        src: 'foo.css',
-        dest: 'bar.css'
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: '<%= globalConfig.build.css %>',
+        src: '*.css',
+        dest: '<%= globalConfig.build.css %>'
       }
-    },*/
+    },
+
+    copy: {
+      govuk_template_css: {
+        cwd: '<%= globalConfig.govuk_template.css_dev %>', // set working folder / root to copy
+        src: '**/*', // copy all files and subfolders
+        dest: '<%= globalConfig.build.css %>', // destination folder
+        expand: true // required when using cwd
+      },
+      govuk_template_img: {
+        cwd: '<%= globalConfig.govuk_template.img_dev %>',
+        src: '**/*',
+        dest: '<%= globalConfig.build.img %>',
+        expand: true
+      }
+    },
+
+    uglify: {
+
+      /*options: {
+        mangle: false
+      },*/
+
+      govuk_template_js: {
+        files: [{
+          expand: true,
+          cwd: '<%= globalConfig.govuk_template.js_dev %>',
+          src: '**/*.js',
+          dest: '<%= globalConfig.build.js %>'
+        }]
+      }
+    },
 
     /*watch: {
       css: {
@@ -58,11 +100,25 @@ module.exports = function(grunt) {
   // css-min task - used to minify straight css files (i.e. the govuk_template files): https://github.com/gruntjs/grunt-contrib-cssmin
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
+  // copy task
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // js uglify
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
 
   // watch task. Does what it says on the tin: https://github.com/gruntjs/grunt-contrib-watch
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Register the various Grunt commands:
   grunt.registerTask('default', ['sass:dev']);
+
+  grunt.registerTask('build', [
+    'copy:govuk_template_css',
+    'cssmin',
+    'sass:build',
+    'uglify',
+    'copy:govuk_template_img',
+  ]);
 
 };
