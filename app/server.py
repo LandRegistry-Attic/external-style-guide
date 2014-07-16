aeef589bc2df0cbe58a4e11120fe0c7a6682c9b3
Demotive <mat@demotive.com>
2014-07-16 13:08:47 +0100

    Setting basic security headers

--- app/server.py
+++ app/server.py
@@ -8,10 +8,19 @@ app = Flask(__name__)
 def asset_path_context_processor():
 	return {'asset_path': '/static/development/govuk-template/'}
 
- 
 @app.route('/')
 def home():
   return render_template('style-guide/index.html')
- 
+
+#  Some useful headers to set to beef up the robustness of the app
+# https://www.owasp.org/index.php/List_of_useful_HTTP_headers
+@app.after_request
+def after_request(response):
+  response.headers.add('Content-Security-Policy', "default-src 'self'; font-src 'self' data:; image-src 'self' data:; script-src 'self' 'unsafe-inline'")
+  response.headers.add('X-Frame-Options', 'deny')
+  response.headers.add('X-Content-Type-Options', 'nosniff')
+  response.headers.add('X-XSS-Protection', '1; mode=block')
+  return response
+
 if __name__ == '__main__':
   app.run(debug=True)
