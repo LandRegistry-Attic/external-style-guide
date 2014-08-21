664425e4be53641a809a6e0931d08b1ac4123c49
Colm Britton <colmjude@gmail.com>
2014-08-21 10:59:57 +0100

    Add task to copy required govuk toolkit javascripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,15 @@
 module.exports = function(grunt) {
 
   var globalConfig = {
+    govuk_toolkit: {
+      js: {
+        path: 'node_modules/govuk_frontend_toolkit/govuk_frontend_toolkit/javascripts/',
+        files: [
+          'govuk/selection-buttons.js',
+          'vendor/polyfills/bind.js'
+        ]
+      }
+    },
     govuk_template: {
       css_dev: 'app/static/development/govuk-template/stylesheets/',
       js_dev: 'app/static/development/govuk-template/javascripts/',
@@ -70,6 +79,12 @@ module.exports = function(grunt) {
     },
 
     copy: {
+      govuk_toolkit_js: {
+        cwd: '<%= globalConfig.govuk_toolkit.js.path %>',
+        src: globalConfig.govuk_toolkit.js.files,
+        dest: 'app/static/development/javascripts',
+        expand: true
+      },
       govuk_template_css: {
         cwd: '<%= globalConfig.govuk_template.css_dev %>', // set working folder / root to copy
         src: '**/*', // copy all files and subfolders
@@ -169,5 +184,8 @@ module.exports = function(grunt) {
     'copy:leaflet_js',
     'cssmin:leaflet_js',
   ]);
+
+  // 3: Update task - copy updates to govuk assets
+  grunt.registerTask('update', ['copy:govuk_toolkit_js']);
 
 };
